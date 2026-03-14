f0efb475c70daec77437e7327641bcaec7d9a01d
Pedro Henrique Silva Rodrigues <ph8464803@gmail.com>
2026-03-14 18:26:21 -0300

    refactor: adjustment to the return code and value (#6)

--- app/controllers/products_controller.ts
+++ app/controllers/products_controller.ts
@@ -4,42 +4,49 @@ import { createProductValidator, updateProductValidator } from '#validators/prod
 import { HttpContext } from '@adonisjs/core/http'
 
 export default class ProductsController {
-  async index({ serialize }: HttpContext) {
-    const products = await Product.query().orderBy('name', 'asc')
-    return serialize(await ProductTransformer.transform(products))
+  async index({ request, serialize }: HttpContext) {
+    const page = Number(request.input('page', 1))
+    const limit = Number(request.input('limit', 10))
+
+    const products = await Product.query().orderBy('name', 'asc').paginate(page, limit)
+    const data = await ProductTransformer.transform(products)
+
+    return serialize(data)
   }
 
   async show({ params, serialize }: HttpContext) {
-    const products = await Product.findOrFail(params.id)
+    const product = await Product.findOrFail(params.id)
 
-    return serialize(await ProductTransformer.transform(products))
+    const data = await ProductTransformer.transform(product)
+
+    return serialize(data)
   }
 
-  async store({ request, serialize }: HttpContext) {
+  async store({ request, response, serialize }: HttpContext) {
     const payload = await request.validateUsing(createProductValidator)
-
     const product = await Product.create(payload)
-    return serialize(await ProductTransformer.transform(product))
+
+    const data = await serialize(ProductTransformer.transform(product))
+
+    return response.created(data)
   }
 
-  async update({ params, request, serialize }: HttpContext) {
-    const product = await Product.findByOrFail(params.id)
-
+  async update({ params, request, response, serialize }: HttpContext) {
+    const product = await Product.findOrFail(params.id)
     const payload = await request.validateUsing(updateProductValidator)
 
     product.merge(payload)
     await product.save()
 
-    return serialize(await ProductTransformer.transform(product))
+    const data = await serialize(ProductTransformer.transform(product))
+
+    return response.ok(data)
   }
 
   async destroy({ params, response }: HttpContext) {
-    const product = await Product.findByOrFail(params.id)
-
+    const product = await Product.findOrFail(params.id)
     await product.delete()
 
-    return response.status(200).send({
-      message: 'Product deleted successfully',
-    })
+    return response.noContent()
   }
 }
