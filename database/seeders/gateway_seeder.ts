6e28cbb1e9f9259701ce712d07c04eae0c1caf69
pedro henrique silva rodrigues <ph8464803@gmail.com>
2026-03-15 01:18:04 -0300

    chore: generating the database and the seeder with the build

--- database/seeders/gateway_seeder.ts
+++ database/seeders/gateway_seeder.ts
@@ -3,15 +3,19 @@ import { BaseSeeder } from '@adonisjs/lucid/seeders'
 
 export default class extends BaseSeeder {
   async run() {
-    await Gateway.create({
-      name: 'Gateway 1',
-      is_active: true,
-      priority: 1,
-    })
-    await Gateway.create({
-      name: 'Gateway 2',
-      is_active: true,
-      priority: 2,
-    })
+    await Gateway.updateOrCreate(
+      { name: 'Gateway 1' },
+      {
+        is_active: true,
+        priority: 1,
+      }
+    )
+    await Gateway.updateOrCreate(
+      { name: 'Gateway 2' },
+      {
+        is_active: true,
+        priority: 2,
+      }
+    )
   }
 }
--- database/seeders/product.ts
+++ database/seeders/product.ts
@@ -3,18 +3,18 @@ import { BaseSeeder } from '@adonisjs/lucid/seeders'
 
 export default class extends BaseSeeder {
   async run() {
-    await Product.createMany([
+    const products = [
       {
         name: 'Plano Pro - Assinatura Mensal',
-        amount: 49.9,
+        amount: 49.99,
       },
       {
         name: 'Curso Desenvolvimento Web Fullstack',
-        amount: 299.9,
+        amount: 299.99,
       },
       {
         name: 'E-book: Como passar em entrevistas técnicas',
-        amount: 19.9,
+        amount: 19.99,
       },
       {
         name: 'Mentoria Exclusiva (1 hora)',
@@ -24,6 +24,10 @@ export default class extends BaseSeeder {
         name: 'Ticket Conferência Tech 2026',
         amount: 450.0,
       },
-    ])
+    ]
+
+    for (const prod of products) {
+      await Product.updateOrCreate({ name: prod.name }, prod)
+    }
   }
 }
