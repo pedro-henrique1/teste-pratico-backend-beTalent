726a0ed6210c18d190880fa10bd538fdbec48db7
pedro henrique silva rodrigues <ph8464803@gmail.com>
2026-03-15 00:30:34 -0300

    refactor: Adjustment to the return value, pagination, and validation

--- app/controllers/clients_controller.ts
+++ app/controllers/clients_controller.ts
@@ -3,19 +3,24 @@ import ClientTransformer from '#transformers/client_transformer'
 import { HttpContext } from '@adonisjs/core/http'
 
 export default class ClientsController {
-  async index({ serialize }: HttpContext) {
-    const clients = await Client.all()
-    return serialize(await ClientTransformer.transform(clients))
+  async index({ serialize, request, response }: HttpContext) {
+    const page = request.input('page', 1)
+    const limit = request.input('limit', 10)
+
+    const clients = await Client.query().orderBy('name', 'asc').paginate(page, limit)
+    return response.ok(await serialize(ClientTransformer.transform(clients)))
   }
 
-  async show({ params, serialize }: HttpContext) {
+  async show({ params, serialize, request, response }: HttpContext) {
+    const limit = request.input('limit', 10)
+
     const client = await Client.query()
       .where('id', params.id)
       .preload('transactions', (q) => {
-        q.orderBy('created_at', 'desc')
+        q.orderBy('created_at', 'desc').limit(limit)
       })
       .firstOrFail()
 
-    return serialize(await ClientTransformer.transform(client))
+    return response.ok(await serialize(ClientTransformer.transform(client)))
   }
 }
--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,26 +1,30 @@
 import User from '#models/user'
 import UserTransformer from '#transformers/user_transformer'
+import { updateUserValidator } from '#validators/user'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class UsersController {
-  async index({ serialize }: HttpContext) {
-    const users = await User.query().orderBy('email', 'asc')
-    return serialize(UserTransformer.transform(users))
+  async index({ serialize, request, response }: HttpContext) {
+    const page = request.input('page', 1)
+    const limit = request.input('limit', 10)
+
+    const users = await User.query().orderBy('email', 'asc').paginate(page, limit)
+    return response.ok(await serialize(UserTransformer.transform(users)))
   }
 
-  async show({ params, serialize }: HttpContext) {
+  async show({ params, serialize, response }: HttpContext) {
     const user = await User.findOrFail(params.id)
-    return serialize(UserTransformer.transform(user))
+    return response.ok(await serialize(UserTransformer.transform(user)))
   }
 
-  async update({ params, request, serialize }: HttpContext) {
+  async update({ params, request, serialize, response }: HttpContext) {
     const user = await User.findOrFail(params.id)
 
-    const data = request.only(['email', 'password', 'role'])
-    await user.merge(data)
+    const payload = await request.validateUsing(updateUserValidator)
+    await user.merge(payload)
     await user.save()
 
-    return serialize(UserTransformer.transform(user))
+    return response.ok(await serialize(UserTransformer.transform(user)))
   }
 
   async destroy({ params, response }: HttpContext) {
